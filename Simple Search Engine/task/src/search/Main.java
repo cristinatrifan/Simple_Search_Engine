6cf0b4d2e1e3bcd2993cc7801fb635e3e2bde603
Cristina Trifan <cristinamariatrifan@gmail.com>
2020-05-13 19:55:51 +0300

    first stage

--- Simple Search Engine/task/src/search/Main.java
+++ Simple Search Engine/task/src/search/Main.java
@@ -3,33 +3,71 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        String[] searchWords = new String[30];
+        ArrayList<String> people = new ArrayList<>();
         String word = " ";
+        int noPeople = 0;
+        int searchQ = 0;
         Main obj = new Main();
 
+        //get all elements and all strings that should be searched in the elements
        try (Scanner scanner = new Scanner(System.in)) {
-           if (scanner.hasNextLine()) {
-               searchWords = scanner.nextLine().split(" ");
+          System.out.println("Enter the number of people: ");
+
+           if (scanner.hasNextInt()) {
+               noPeople = scanner.nextInt();
            }
-           if (scanner.hasNextLine()) {
-               word = scanner.nextLine();
+
+           System.out.println("Enter all people: ");
+           scanner.nextLine();
+
+           while (scanner.hasNextLine() && noPeople > 0) {
+                people.add(scanner.nextLine());
+                noPeople--;
            }
+
+            System.out.println("Enter the number of search queries: ");
+
+           if (scanner.hasNextLine()) {
+               searchQ = Integer.parseInt(scanner.nextLine());
+           }
+
+           while (searchQ > 0) {
+               System.out.println("");
+               System.out.println("Enter data to search people: ");
+
+               if (scanner.hasNextLine()) {
+                   word = scanner.nextLine().trim();
+               }
+               obj.getMatch(people, word);
+               searchQ--;
+           }
+
        }
        catch (Exception e) {
            System.out.println("Problems with reading the input");
        }
-
-       System.out.println(obj.getIndexOfWord(searchWords, word));
     }
 
-    private String getIndexOfWord(String[] inputList, String searchedWord) {
-        String text = " ";
-        for (int i = 0; i < inputList.length; i++) {
-            if (inputList[i].equals(searchedWord)) {
-                Integer index = i + 1;
-                return text = index.toString();
+    private void getMatch(ArrayList<String> people, String word) {
+        ArrayList<String> matches = new ArrayList<>();
+
+        //search for the strings in the elements
+        for (int i = 0; i < people.size(); i++) {
+               if (Arrays.stream(people.get(i)
+                       .toLowerCase().split(" "))
+                       .anyMatch(s -> s.contains(word.toLowerCase()))) {
+                   matches.add(people.get(i));
+               }
+           }
+
+        //print the output
+        if (matches.size() > 0) {
+            System.out.println("");
+            System.out.println("Found people: ");
+            for (int i = 0; i < matches.size(); i++) {
+                System.out.println(matches.get(i));
             }
+        } else {
+            System.out.println("No matching people found.");
         }
-        return text = "Not found";
-    }
-}
+}}
